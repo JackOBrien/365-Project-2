89634002b9b6e4e34e6f14a4c2ec5c2e1e417bcc
Jack O'Brien <obrienjo@mail.gvsu.edu>
2016-03-14 17:23:04 -0400

    Added class facts

---
+++ project2.pl
@@ -0,0 +1,25 @@
+/* Facts */
+class(467,CS Project,10:00 am - 10:50 am,MWF,MAK B1118,Dr. Engelsma,CS).
+class(463,IS Project,2:00 pm - 2:50 pm,MWF,MAK D2123,Mr. Lange,IS).
+class(460,MIS,10:00 am - 11:15 am,TR,MAK B1116,Dr. P. Leidig,IS).
+class(457,Data Communications,2:00 pm - 2:50 pm,MWF,MAK D1117,Dr. Kalafut,CS).
+class(452,OS Concepts,1:00 pm - 1:50 pm,MWF,MAK D1117,Dr. Wolffe,CS).
+class(451,Computer Architecture,10:00 am - 10:50 am,MWF,MAK B1118,Dr. Kurmas,CS).
+class(450,IS Project Management,12:00 pm - 12:50 pm,MWF,MAK D1117,Dr. Schymik,IS).
+class(443,Software Development Tools,11:00 am - 11:50 am,MWF,MAK B1124,Ms. Peterman,IS).
+class(437,Distributed Computing,10:00 am - 10:50 am,MWF,MAK B1118,Dr. Engelsma,CS).
+class(375,Wireless Networking Systems,6:00 pm - 7:50 pm,R,EC 612,Dr. El-Said,IS).
+class(371,Web Applicatino Programming,4:00 pm - 5:15 pm,MW,MAK D1117,Dr. Scripps,CS).
+class(365,AI,10:00 am - 11:15 am,TR,MAK D1117,Dr. J. Leidig,CS).
+class(361,System Programming,4:00 pm - 5:15 pm,TR,MAK B1116,Dr. Trefftz,CS).
+class(358,Information Insurance,3:00 pm - 3:50 pm,MWF,MAK A1105,Dr. Kalafut,CS).
+class(353,Database,12:00 pm - 12:50 pm,MWF,MAK B1118,Dr. Alsabbagh,CS).
+class(350,Software Engineering,10:00 am - 10:50 am,MWF,MAK D1117,Dr. Jorgensen,CS).
+class(343,Structure of Programming Languages,1:00 pm - 1:50 pm,MWF,MAK B1124,Dr. Nandigam,CS).
+class(339,IT Project Management,1:00 pm - 2:15 pm,TR,MAK A1105,Mr. Lange,IS).
+class(337,Network Systems Management,3:00 pm - 3:50 pm,TR,MAK B1124,Dr. El-Said,IS).
+class(333,DB Management and Implementation,6:00 pm - 8:50 pm,W,MAK D1117,Ms. Posada,IS).
+class(330,Systems Analysis and Design,9:00 am - 9:50 am,MWF,MAK D1117,Dr. Du,IS).
+class(661,Medical and BioInformatics,6:00 pm - 8:50 pm,T,EC 612,Dr. J. Leidig,CIS).
+class(671,Information Visualization,6:00 pm - 8:50 pm,R,EC 612,Dr. J. Leidig,CIS).
+class(691,MBI Capstone,6:00 pm - 8:50 pm,M,EC 612,Dr. J. Leidig,CIS).
